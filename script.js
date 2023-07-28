586df17eefe61553d706b5f7678e92d66f90c545
Anilkumar8848 <88019495+Anilkumar8848@users.noreply.github.com>
2023-07-28 14:40:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let timeRef = document.querySelector(".timer-display");
 let int = null;
 
 document.getElementById("start-timer").addEventListener("click", () => {
-    if(int !== null) {
+    if (int !== null) {
         clearInterval(int);
     }
     int = setInterval(displayTimer, 10);
@@ -17,17 +17,17 @@ document.getElementById("reset-timer").addEventListener("click", () => {
     clearInterval(int);
     [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
     timeRef.innerHTML = "00 : 00 : 00 : 000 ";
-}); 
+});
 
 function displayTimer() {
     milliseconds += 10;
-    if(milliseconds == 1000) {
+    if (milliseconds == 1000) {
         milliseconds = 0;
         seconds++;
-        if(seconds == 60) {
+        if (seconds == 60) {
             seconds = 0;
             minutes++;
-            if(minutes == 60) {
+            if (minutes == 60) {
                 minutes = 0;
                 hours++;
             }
@@ -37,13 +37,12 @@ function displayTimer() {
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = 
+    let ms =
         milliseconds < 10
-        ? "00" + milliseconds
-        : milliseconds < 100
-        ? "0" + milliseconds
-        : milliseconds;
+            ? "00" + milliseconds
+            : milliseconds < 100
+            ? "0" + milliseconds
+            : milliseconds;
 
     timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
-
 }
